4b155b4063508efd24c85825b1a06b807486142b
flexicious <flexicious info@flexicious.com>
2014-10-22 18:39:36 +0200

    Initial commit

---
+++ task.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Ibrahim on 22/10/2014.
+ */
+var tasks=[
+    {id:1,title:'task1',description :'memo task 1',status:'closed',statusId:0},
+    {id:2,title:'task2',description :'memo task 2',status:'open',statusId:1},
+    {id:3,title:'task3',description :'memo task 3',status:'closed',statusId:0},
+    {id:4,title:'task4',description :'memo task 4',status:'open',statusId:1},
+    {id:5,title:'task5',description :'memo task 5',status:'open',statusId:1},
+    {id:6,title:'task6',description :'memo task 6',status:'new',statusId:2},
+    {id:7,title:'task7',description :'memo task 7',status:'new',statusId:2},
+    {id:8,title:'task8',description :'memo task 6',status:'new',statusId:2},
+    {id:9,title:'task9',description :'memo task 9',status:'closed',statusId:0},
+    {id:10,title:'task10',description :'memo task 10',status:'closed',statusId:0},
+    {id:11,title:'task11',description :'memo task 11',status:'new',statusId:2},
+    {id:12,title:'task12',description :'memo task 12',status:'closed',statusId:0}
+]
